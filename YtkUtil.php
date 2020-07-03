7da372dae54fc6c2445e29d5c6ff5a8e45ea6156
Andreas Pott <andreasapott@googlemail.com>
2020-07-03 21:49:37 +0200

    Added support for schemes and scheme analysis

--- YtkUtil.php
+++ YtkUtil.php
@@ -75,4 +75,58 @@ class YtkUtil {
         return $result;
     }
     
+    /* the proposal for schemes definitions
+     * the *identity* scheme simply required that a model implements the attribute id as primary key, thus allowing to identify items in the tables in a unique way
+     * the *journaling* scheme implies that the record keeps track of its creation and last update timestamp (in unit time format).
+     * the *naming* requires records to implement both a name and a description
+     * the *ownership* scheme requires the item to be assigned to user. This is a foreign key constraint and implicitly requires the active record to support users
+     * 
+     * More schemes may be use in the future: e.g. validity, user_comment, historization
+     */
+    public static function getSchemes() 
+    {
+        return array(
+            'identity' => array('id'),
+            'journaling' => array('created','changed'),
+            'naming' => array('name','desc'),
+            'ownership' => array('user_id'),
+        );
+    }
+
+    /* Analyze if the ActiveRecord $item conforms the data fields defined by $scheme.
+     * return  1 if the class implements the full scheme
+     * return  0 if the class partially implements the scheme
+     * return -1 if the class does not implement the scheme at all
+     * return -1 if $item is not derived from CActiveRecord
+     */
+    public static function isScheme($scheme, $item) 
+    {
+        // if $item is a string, we check if $item is a class name and create an instance of this class
+        if (is_string($item)) {
+            if (class_exists($item))
+                $item = new $item;
+            else 
+                return -1;
+        }
+
+        if (!$item instanceof CActiveRecord)
+            return -1;
+        if (is_string($scheme)) {
+            // lookup the scheme in our database
+            $schemes = self::getSchemes();
+            if (!array_key_exists($scheme, $schemes))
+                return -1;
+            $scheme = $schemes[$scheme];
+        } 
+        else if (!is_array($scheme))
+            return -1;
+        // compare the attributs of the scheme with those of the active record-derived class
+        $res = array_intersect($scheme, array_keys($item->attributes));
+        if (count($res)==0)
+            return -1;
+        if (count($res)==count($scheme))
+            return 1;
+        return 0;
+    }
+
 }
