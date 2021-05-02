4cba490693d32bc1c7786d93810160fd94e24e37
Andreas Pott <andreasapott@googlemail.com>
2021-05-02 11:46:00 +0200

    MiniPager can now customize the size of the button

--- YtkMiniPager.php
+++ YtkMiniPager.php
@@ -30,37 +30,43 @@
  */
 class YtkMiniPager extends CWidget { 
     // the name as string of the model class derived from CActiveRecord on which we navigate
-    public $model;             
+    public $model;
 
     // a model instance derived from CActiveRecord. The model must implment an attribute 
     // named id; pass the current model in a typical item view scenario
-    public $item;              
-    
+    public $item;
+
     // the url to call when the button is clicked; the url must accept a GET parameter 
     // named id to pass the id of the element
-    public $url;               
+    public $url;
+
+    // the size of the minipages as passed to TbButtonGroup control. Defaults to size 'normal'
+    public $buttonSize;
 
     // optional parameter: additional condition to filter for when determining next, 
     // prev, total index, and total item count
-    public $filter;            
-    
+    public $filter;
+
     // optional parameter: mixed | boolean or array: show direkt link options as dropdown 
     // on the middle button
-    public $links;             
+    public $links;
 
 
     // assign default values for unset attributes
-    public function init() { 
+    public function init() {
         parent::init(); 
         // assign default values for those properties which are not defined in widget init
-        if ($this->model === null) 
+        if ($this->model === null)
             $this->model = '';
+        // assign default values for those properties which are not defined in widget init
+        if ($this->buttonSize === null)
+            $this->buttonSize = 'normal';
         if ($this->filter === null)
             $this->filter = 'TRUE';     // if we have no specific filter condition we use TRUE as 
                                         // this condition works both as placeholder in empty 
                                         /// condition and as optional condition with AND 
         if ($this->links === null)
-            $this->links = false;     
+            $this->links = false;
         } 
 
     // render the widget by using echo to print the desired content
@@ -72,7 +78,7 @@ class YtkMiniPager extends CWidget {
         $res = $model::model()->findAll(array(
             'condition'=>$this->filter.' AND id > '.$this->item->id));
         $next_id = count($res)>0 ? $res[0]->id : 0;
-        
+
         // find previous model before the current one
         $res = $model::model()->findAll(array(
             'condition'=>$this->filter.' AND id < '.$this->item->id,
@@ -95,14 +101,14 @@ class YtkMiniPager extends CWidget {
 
         // generate all three buttons as a button group
         $this->widget('bootstrap.widgets.TbButtonGroup', array(
-            'size'=>'normal', // null, 'large', 'small' or 'mini'
+            'size'=>$this->buttonSize, // null, 'large', 'small' or 'mini'
             'type'=>'null', // null, 'primary', 'info', 'success', 'warning', 'danger' or 'inverse'	
             'buttons'=>array(
                 array('icon'=>'chevron-left', 'label'=>' ', 'url'=>$prev_id>0 ? array($this->url, 'id'=>$prev_id) : '#', 'active'=>$prev_id == 0,),
                 array('label'=>$idx.' / '.$cnt, 'size'=>'small', 'url'=>'#', 'active'=>true, 'items'=>$dirLinks,),
                 array('icon'=>'chevron-right', 'label'=>' ', 'url'=>$next_id>0 ? array($this->url, 'id'=>$next_id) : '#', 'active'=>$next_id == 0,),
             ),
-        ));        
+        ));
         echo '</div>';
     } 
 } 
